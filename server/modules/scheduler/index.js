97d54db50dabea8696f0b833ec4064b075bdd9c3
Florian Riedel <fochlac@gmail.com>
2017-11-22 10:07:21 +0100

    fix scheduler, add logging

--- server/modules/scheduler/index.js
+++ server/modules/scheduler/index.js
@@ -1,6 +1,7 @@
 const 	scheduler 	= require('node-schedule')
 	,	mail 		= require(process.env.FOOD_HOME + 'modules/mailer')
 	,	mealDb 		= require(process.env.FOOD_HOME + 'modules/db/meals')
+    ,   log         = require(process.env.FOOD_HOME + 'modules/log')
     ,   error       = require(process.env.FOOD_HOME + 'modules/error');
 
 const second = 1000,
@@ -16,25 +17,36 @@ const mealDeadline = (meal) => () => {
 	// trigger push
 }
 
-
 module.exports = {
 	init: () => {
 		mealDb.getAllMeals()
 			.then((meals) => {
 				meals.forEach(meal => {
-					schedule['meal_' + meal.id] = scheduler.scheduleJob(new Date(meal.deadline - hour * 2), mealDeadline)
+					if (Date.now() < +deadline) {
+						let date = new Date(meal.deadline - hour * 2);
+						log(6, `scheduling deadline reminder for ${meal.name} at ${date.getDate()}.${date.getMonth() + 1} - ${date.getHours()}:${date.getMinutes()}`);
+						schedule['meal_' + meal.id] = scheduler.scheduleJob(date, mealDeadline)
+					}
 				});
-			})
-			.catch(error.promise(2, 'error setting up scheduler'));
+			});
 	},
 	scheduleMeal: meal => {
-		schedule['meal_' + meal.id] = scheduler.scheduleJob(new Date(meal.deadline - hour * 2), mealDeadline);
+		let date = new Date(meal.deadline - hour * 2);
+		log(6, `scheduling deadline reminder for ${meal.name} at ${date.getDate()}.${date.getMonth() + 1} - ${date.getHours()}:${date.getMinutes()}`);
+		schedule['meal_' + meal.id] = scheduler.scheduleJob(date, mealDeadline)
 	},
 	rescheduleMeal: meal => {
-		schedule['meal_' + meal.id].cancel();
-		schedule['meal_' + meal.id] = scheduler.scheduleJob(new Date(meal.deadline - hour * 2), mealDeadline);
+		let date = new Date(meal.deadline - hour * 2);
+		log(6, `rescheduling deadline reminder for ${meal.name} at ${date.getDate()}.${date.getMonth() + 1} - ${date.getHours()}:${date.getMinutes()}`);
+		if (schedule['meal_' + id]) {
+			schedule['meal_' + id].cancel();
+		}
+		schedule['meal_' + meal.id] = scheduler.scheduleJob(date, mealDeadline)
 	},
 	cancelMeal : id => {
-		schedule['meal_' + meal.id].cancel();
+		log(6, `canceling deadline reminder for meal_${id}`);
+		if (schedule['meal_' + id]) {
+			schedule['meal_' + id].cancel();
+		}
 	}
 }
