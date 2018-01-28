95e8c64ba82e39a27dd9558455ee95c1b3edff09
Florian Riedel <fochlac@gmail.com>
2018-01-28 03:13:31 +0100

    asd

--- test/client/uitests/register_login.spec.js
+++ test/client/uitests/register_login.spec.js
@@ -108,7 +108,7 @@ describe('login area', () => {
     });
 
     it('should remain logged in on page refresh', async function() {
-        this.timeout(5000);
+        this.timeout(10000);
 
         await this.driver.refresh();
 
