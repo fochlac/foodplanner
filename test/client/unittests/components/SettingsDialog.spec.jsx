65868355459bdf081ad1e6a96a07a7f3b10c924f
Florian Riedel <f.riedel@epages.com>
2018-02-05 01:50:27 +0100

    Fp 75 (#162)
    * FP-75: add restriction information
    
    * FP-75: add fix tests and bugs

--- test/client/unittests/components/SettingsDialog.spec.jsx
+++ test/client/unittests/components/SettingsDialog.spec.jsx
@@ -153,6 +153,9 @@ describe('SettingsDialog', () => {
     expect(wrapper.find('.submit').prop('disabled')).toBe(true)
     expect(wrapper.find('.body #SettingsDialog_pass2.invalid')).toHaveLength(1)
     expect(wrapper.find('.body #SettingsDialog_pass.invalid')).toHaveLength(1)
+    wrapper.find('.body #SettingsDialog_pass2').simulate('change', { target: { value: 'as' } })
+    expect(wrapper.find('.submit').prop('disabled')).toBe(true)
+    expect(wrapper.find('.body #SettingsDialog_pass.invalid')).toHaveLength(0)
     wrapper.find('.body #SettingsDialog_pass2').simulate('change', { target: { value: 'asd' } })
     expect(wrapper.find('.submit').prop('disabled')).toBe(false)
     expect(wrapper.find('.body #SettingsDialog_pass.invalid')).toHaveLength(0)
@@ -209,8 +212,97 @@ describe('SettingsDialog', () => {
     }, 100)
   })
 
+  test('should display changed notification settings', done => {
+    let save_settings = false
+    const wrapper = shallow(
+      <SettingsDialog user={{}} create_error={() => null} delete_error={() => null} save_settings_locally={state => (save_settings = state)} />,
+    )
+    window.Notification = { permission: 'granted' }
+    wrapper.setProps({ user: { creationNotice_notification: 1 } })
+    wrapper.update()
+    wrapper.setProps({ user: { deadlineReminder_notification: 1 } })
+    wrapper.update()
+
+    wrapper.find('button.submit').simulate('click')
+    setTimeout(() => {
+      expect(save_settings).toEqual({
+        id: undefined,
+        mail: '',
+        name: '',
+        creationNotice: 0,
+        creationNotice_notification: 0,
+        deadlineReminder: 0,
+        deadlineReminder_notification: 1,
+        pass: '',
+        pass2: '',
+      })
+      done()
+    }, 100)
+  })
+
+  test('should not  display unchanged notification settings', done => {
+    let save_settings = false
+    const wrapper = shallow(
+      <SettingsDialog
+        user={{ creationNotice_notification: 1, deadlineReminder_notification: 0 }}
+        create_error={() => null}
+        delete_error={() => null}
+        save_settings_locally={state => (save_settings = state)}
+      />,
+    )
+    window.Notification = { permission: 'granted' }
+    wrapper.setProps({ user: { creationNotice_notification: 1, deadlineReminder_notification: 0 } })
+    wrapper.update()
+
+    wrapper.find('button.submit').simulate('click')
+    setTimeout(() => {
+      expect(save_settings).toEqual({
+        id: undefined,
+        mail: '',
+        name: '',
+        creationNotice: 0,
+        creationNotice_notification: 1,
+        deadlineReminder: 0,
+        deadlineReminder_notification: 0,
+        pass: '',
+        pass2: '',
+      })
+      done()
+    }, 100)
+  })
+
+  test('should display changed notification settings', done => {
+    let save_settings = false,
+      timeoutFunction,
+      tmp = global.window.setTimeout
+
+    global.window.setTimeout = cb => (timeoutFunction = cb)
+
+    const wrapper = shallow(
+      <SettingsDialog
+        user={{ creationNotice_notification: 1, deadlineReminder_notification: 0 }}
+        create_error={() => (save_settings = 'create_error')}
+        delete_error={() => (save_settings = 'delete_error')}
+        save_settings_locally={state => (save_settings = state)}
+      />,
+    )
+    window.Notification = { permission: 'denied' }
+
+    wrapper.find('button.submit').simulate('click')
+
+    const testInterval = setInterval(() => {
+      expect(save_settings).toBe('create_error')
+      timeoutFunction()
+      expect(save_settings).toBe('delete_error')
+      global.window.setTimeout = tmp
+      clearInterval(testInterval)
+      done()
+    }, 100)
+  })
+
   test('should send changed values on submit', done => {
     let save_settings = false
+    window.Notification = { permission: 'granted' }
     const wrapper = shallow(<SettingsDialog user={{}} save_settings_locally={state => (save_settings = state)} />)
 
     wrapper.find('.notificationMatrix .notification input').forEach(elem => {
