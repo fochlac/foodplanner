b4aae1ce2a7a81cb247e36d5804458612879d7c8
Florian Riedel <f.riedel@epages.com>
2018-02-05 04:31:28 +0100

    Fix bug (#169)
    * fix bug
    
    * fix history
    
    * fix history
    
    * fix history

--- client/components/reducers/app.js
+++ client/components/reducers/app.js
@@ -1,24 +1,28 @@
 const app = (state = {}, action) => {
   switch (action.type) {
     case 'DIALOG':
-      return { ...state, dialog: { type: action.content, option: action.option } };
+      return { ...state, dialog: { type: action.content, option: action.option } }
     case 'HISTORY':
-      return action.app;
+      action.app.errors = {}
+      delete action.app.mailSuggestion
+      action.app.busy = false
+      action.app.hiddenBusy = false
+      return action.app
     case 'CHECK_MAIL':
       if (action.status === 'complete' && !action.data.error) {
-        return { ...state, mailSuggestion: action.data };
+        return { ...state, mailSuggestion: action.data }
       } else if (action.status === 'complete') {
-        return { ...state, mailSuggestion: undefined };
+        return { ...state, mailSuggestion: undefined }
       }
-      return state;
+      return state
 
     case 'SEND_MONEY':
     case 'SIGNOUT':
     case 'SAVE_SETTINGS':
       if (action.type !== 'SEND_MONEY' || (action.status === 'complete' && !action.data.error)) {
-        return { ...state, mailSuggestion: undefined, dialog: { type: "" } };
+        return { ...state, mailSuggestion: undefined, dialog: { type: '' } }
       }
-      return state;
+      return state
 
     case 'MEAL_SIGNUP':
     case 'MEAL_EDIT':
@@ -29,33 +33,33 @@ const app = (state = {}, action) => {
     case 'SIGNIN':
     case 'REGISTER':
       if (action.status === 'complete') {
-        return { ...state, dialog: { type: "" } };
+        return { ...state, dialog: { type: '' } }
       }
-      return state;
+      return state
     case 'BUSY':
-      return { ...state, busy: action.state };
+      return { ...state, busy: action.state }
     case 'HIDDEN_BUSY':
-      return { ...state, hiddenBusy: action.state };
+      return { ...state, hiddenBusy: action.state }
     case 'POSTMESSAGE':
       if (action.message === 'offline') {
-        return { ...state, offline: action.payload.state };
+        return { ...state, offline: action.payload.state }
       }
-      return state;
+      return state
     case 'SHOW_ERROR':
-      return { ...state, errors: { ...state.errors, [action.id]: action.content } };
+      return { ...state, errors: { ...state.errors, [action.id]: action.content } }
     case 'DELETE_ERROR':
-      let errors = { ...state.errors };
-      delete errors[action.id];
-      return { ...state, errors: errors };
+      let errors = { ...state.errors }
+      delete errors[action.id]
+      return { ...state, errors: errors }
     case 'REFRESH':
-      if (action.status === "complete" && action.data.version) {
-        return { ...state, dataversion: action.data.version };
+      if (action.status === 'complete' && action.data.version) {
+        return { ...state, dataversion: action.data.version }
       }
-      return state;
+      return state
 
     default:
-      return state;
+      return state
   }
 }
 
-export default app;
+export default app
