0753c2d7c97d5320a1eb9c3a16644fa322b2bfd1
ep-friedel <friedel@epages.com>
2018-02-26 01:02:08 +0100

    FP-195: parse markdown links fix regex

--- client/utils/markdown.js
+++ client/utils/markdown.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const regex = {
-  linkGlobal: /\[[^\(\)]*\]\([^\[\]]*\)/g,
-  link: /\[([^\(\)]*)\]\(([^\[\]]*)\)/,
+  linkGlobal: /\[[^\[\]*\]\([^\(\)]*\)/g,
+  link: /\[([^\[\]*)\]\(([^\(\)]*)\)/,
 }
 
 const parseLink = string => {
--- test/client/unittests/scripts/markdown.spec.js
+++ test/client/unittests/scripts/markdown.spec.js
@@ -12,4 +12,14 @@ describe('replaceLinks', () => {
     expect(link.prop('href')).toBe('http://test.de/')
     expect(link.text()).toBe('test123')
   })
+  test('should create link elements from markdown links', () => {
+    const wrapper = shallow(<div>{replaceLinks('test123 asdas [test123](http://test.de/) [test2](test2) )(')}</div>)
+    const link = wrapper.find('a')
+
+    expect(link).toHaveLength(2)
+    expect(link.at(0).prop('href')).toBe('http://test.de/')
+    expect(link.at(0).text()).toBe('test123')
+    expect(link.at(1).prop('href')).toBe('test2')
+    expect(link.at(1).text()).toBe('test2')
+  })
 })
