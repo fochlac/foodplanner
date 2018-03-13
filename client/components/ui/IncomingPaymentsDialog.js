fa51eb14bb2598c1eea1d07d5b5cea3212a09138
ep-friedel <friedel@epages.com>
2018-03-14 00:01:44 +0100

    FP-216:  get old meals for incoming payments dialog

--- client/components/ui/IncomingPaymentsDialog.js
+++ client/components/ui/IncomingPaymentsDialog.js
@@ -1,11 +1,15 @@
-import { close_dialog, send_money, toggle_paid } from 'STORE/actions.js'
+import { close_dialog, load_history, send_money, toggle_paid } from 'STORE/actions.js'
 
 import IncomingPaymentsDialog from 'UI/IncomingPaymentsDialog/IncomingPaymentsDialog.jsx'
 import React from 'react'
 import { connect } from 'react-redux'
 
 const mapStateToProps = (state, ownProps) => {
-  const historyLoaded = Array(state.app.historySize).fill().slice(0, 20).map((val, index) => state.historyMealMap[index]).every(id => id)
+  const historyLoaded = Array(state.app.historySize)
+    .fill()
+    .slice(0, 20)
+    .map((val, index) => state.historyMealMap[index])
+    .every(id => id)
   return {
     userId: state.user.id,
     meals: state.meals,
@@ -13,7 +17,5 @@ const mapStateToProps = (state, ownProps) => {
     historyLoaded,
   }
 }
-
-
 
 export default connect(mapStateToProps, { close_dialog, send_money, toggle_paid, load_history })(IncomingPaymentsDialog)
