69c66ecc726cb19a8ceb7bf2f50a7918706b574e
ep-friedel <friedel@epages.com>
2018-03-16 01:03:55 +0100

    FP-178: Administration Page

--- server/router/controller/instance.js
+++ server/router/controller/instance.js
@@ -3,6 +3,7 @@ const instanceDB = require(process.env.FOOD_HOME + 'modules/db/instance'),
   userDB = require(process.env.FOOD_HOME + 'modules/db/user'),
   caches = require(process.env.FOOD_HOME + 'modules/cache'),
   request = require('request-promise-native'),
+  error = require(process.env.FOOD_HOME + 'modules/error'),
   jwt = require(process.env.FOOD_HOME + 'modules/auth/jwt'),
   proxy = `http://localhost:${process.env.PROXY_PORT}/proxies`,
   cookieOptions = { secure: process.env.DEVELOP ? false : true, httpOnly: true, expires: new Date(Date.now() + 1000 * 3600 * 24 * 365) }
@@ -14,49 +15,57 @@ let cache = caches.getCache('users'),
 
 module.exports = {
   checkDomainTaken: async (req, res) => {
-    const { subdomain } = req.query
-    const subdomains = Object.keys(
-      await request({
-        uri: proxy,
-        json: true,
-      }),
-    ).map(domain => domain.split('.')[0])
+    try {
+      const { subdomain } = req.query
+      const subdomains = Object.keys(
+        await request({
+          uri: proxy,
+          json: true,
+        }),
+      ).map(domain => domain.split('.')[0])
 
-    res.status(200).send(
-      !subdomains.includes(subdomain)
-        ? {
-            isValid: true,
-            name: subdomain,
-          }
-        : { isValid: false },
-    )
+      res.status(200).send(
+        !subdomains.includes(subdomain)
+          ? {
+              isValid: true,
+              name: subdomain,
+            }
+          : { isValid: false },
+      )
+    } catch (err) {
+      err.router.internalError(res)(err)
+    }
   },
 
   createInstance: async (req, res) => {
-    const [instance, userCrypt] = await Promise.all([instanceDB.createInstance(req.body), crypto.createUserHash(req.body.hash)])
-    const [user, subdomain] = await Promise.all([
-      userDB.createUser({ ...req.body, instance: instance.id, admin: true }, userCrypt.hash, userCrypt.salt),
-      request({
-        uri: proxy,
-        method: 'POST',
-        json: true,
-        body: {
-          host: req.body.subdomain + '.fochlac.com',
-          proxy: {
-            redirect: false,
-            url: '/' + instance.id,
-            port: 'FOOD_PORT',
+    try {
+      const [instance, userCrypt] = await Promise.all([instanceDB.createInstance(req.body), crypto.createUserHash(req.body.hash)])
+      const [user, subdomain] = await Promise.all([
+        userDB.createUser({ ...req.body, instance: instance.id, admin: true }, userCrypt.hash, userCrypt.salt),
+        request({
+          uri: proxy,
+          method: 'POST',
+          json: true,
+          body: {
+            host: req.body.subdomain + '.fochlac.com',
+            proxy: {
+              redirect: false,
+              url: '/' + instance.id,
+              port: 'FOOD_PORT',
+            },
           },
-        },
-      }),
-    ])
+        }),
+      ])
 
-    mailCache.deleteAll()
-    userListCache.deleteAll()
+      mailCache.deleteAll()
+      userListCache.deleteAll()
 
-    const token = await jwt.createToken(user)
+      const token = await jwt.createToken(user)
 
-    res.cookie('jwt', token, cookieOptions)
-    res.status(200).send({ user, instance })
+      res.cookie('jwt', token, cookieOptions)
+      res.status(200).send({ user, instance })
+    } catch (err) {
+      err.router.internalError(res)(err)
+    }
   },
 }
--- server/router/routes/api/instance.js
+++ server/router/routes/api/instance.js
@@ -1,7 +1,6 @@
 const instance = require('express').Router(),
   instanceController = require(process.env.FOOD_HOME + 'router/controller/instance'),
   jwt = require(process.env.FOOD_HOME + 'modules/auth/jwt'),
-  error = require(process.env.FOOD_HOME + 'modules/error'),
   validate = require(process.env.FOOD_HOME + 'middleware/validate')
 
 instance.get(
@@ -9,7 +8,7 @@ instance.get(
   validate('query', {
     subdomain: /^[_A-Za-z0-9\-]{4,100}$/,
   }),
-  (req, res) => instanceController.checkDomainTaken(req, res).catch(error.router.internalError(res)),
+  instanceController.checkDomainTaken,
 )
 
 instance.post(
@@ -22,7 +21,7 @@ instance.post(
     company: 'utf8',
     subdomain: /^[_A-Za-z0-9\-]{4,100}$/,
   }),
-  (req, res) => instanceController.createInstance(req, res).catch(error.router.internalError(res)),
+  instanceController.createInstance,
 )
 
 module.exports = instance
